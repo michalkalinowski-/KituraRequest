16693093afcfdd15b1e02b86ea27aadf249c1240
mkalinowski <michal.kalinowski@amadeus.com>
2016-06-18 16:11:01 +0200

    bumps kitura-net version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "KituraRequest",
     dependencies: [
-            .Package(url: "https://github.com/michalkalinowski-/Kitura-net.git", majorVersion: 0, minor: 15),
+            .Package(url: "https://github.com/michalkalinowski-/Kitura-net.git", majorVersion: 0, minor: 17),
         ]
 )
