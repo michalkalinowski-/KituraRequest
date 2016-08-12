80e1afdb8f4cda8aa6dc714fb2bf64ddb46d953b
mkalinowski <michal.kalinowski@amadeus.com>
2016-08-12 10:15:07 +0200

    bumps swift version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "KituraRequest",
     dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 21),
+            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 26),
         ]
 )
